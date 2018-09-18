456908365a90dc64a28efee0c838bd4901693674
Jackson <jackson.hardaker@gmail.com>
2018-09-18 11:39:54 -0400

    Add scrape script

---
+++ scrape.js
@@ -0,0 +1,106 @@
+const rp = require('request-promise');
+const $ = require('cheerio');
+const fs = require('fs');
+
+const wikiBase = 'https://en.wikipedia.org';
+const entryPoint =
+  '/w/index.php?title=Category:Visa_requirements_by_nationality';
+
+(async function() {
+  let entryPointHtml = await rp(`${wikiBase}${entryPoint}`);
+  let nextLink = $(
+    $(
+      'a[title|="Category:Visa requirements by nationality"]',
+      entryPointHtml
+    )[0]
+  ).attr('href');
+  let nextPageHtml = await rp(`${wikiBase}${nextLink}`);
+
+  const countryLinks = [
+    ...getCountryVisaPageLinks(entryPointHtml),
+    ...getCountryVisaPageLinks(nextPageHtml)
+  ];
+
+  const requirementsArray = await getVisaRequrementsObjects(countryLinks);
+
+  const jsonData = JSON.stringify(requirementsArray, null, 4);
+  const jsonDataMin = JSON.stringify(requirementsArray);
+  fs.writeFile('visaRequirements.json', jsonData, 'utf8', () => {});
+  fs.writeFile('visaRequirements.min.json', jsonDataMin, 'utf8', () => {});
+})();
+
+async function getVisaRequrementsObjects(urls) {
+  const visaReqArr = [];
+
+  for (let i = 0; i < urls.length; i++) {
+    console.log(`[Beginning Scrape: ${urls[i]}]`);
+
+    let requirementsObj = await getCountryVisaRequirements(urls[i]);
+
+    visaReqArr.push({
+      scrapeUrl: urls[i],
+      requirements: requirementsObj
+    });
+
+    console.log(`[Finished Scrape: ${urls[i]}]`);
+  }
+
+  return visaReqArr;
+}
+
+async function getCountryVisaRequirements(url) {
+  const html = await rp(`${wikiBase}${url}`);
+  const visaReqObj = {};
+
+  const visaReqTables = $('table.wikitable', html);
+
+  if (visaReqTables.length > 0) {
+    const visaReqRows = Array.from($(visaReqTables[0]).find('tr'));
+
+    visaReqRows.forEach(row => {
+      let visaReqCells = $(row).find('td');
+
+      let countryName = visaReqCells[0]
+        ? $(visaReqCells[0])
+            .text()
+            .trim()
+        : null;
+      let status = visaReqCells[1]
+        ? $(visaReqCells[1])
+            .text()
+            .trim()
+            .split('[')[0]
+        : null;
+      let duration = visaReqCells[2]
+        ? $(visaReqCells[2])
+            .text()
+            .trim()
+        : null;
+      let notes = visaReqCells[3] 
+        ? $(visaReqCells[3])
+            .text()
+            .replace(/\[\d+\]/g, '')
+            .trim()
+        : null;
+
+      if (countryName) {
+        visaReqObj[countryName.toLowerCase().replace(/\s/g, '_')] = {
+          country: countryName,
+          status: status,
+          allowedStay: duration,
+          notes: notes
+        };
+      }
+    });
+  }
+
+  return visaReqObj;
+}
+
+function getCountryVisaPageLinks(html) {
+  const linksOnly = Array.from($('div.mw-category', html).find('a')).map(
+    link => link.attribs.href
+  );
+
+  return linksOnly;
+}
